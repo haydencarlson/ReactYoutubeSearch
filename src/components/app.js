3b1604223a466cb8073f2931d43572556eb96469
Hayden Carlson <haydog2@hotmail.com>
2016-11-04 19:39:56 +0000

    Youtube Search App using api with react

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,48 @@
 import React, { Component } from 'react';
+import SearchBar from './search_bar';
+import ReactDOM from 'react-dom';
+import VideoList from './video_list';
+import YTSearch from 'youtube-api-search';
+import VideoDetail from './video_detail';
+import _ from 'lodash';
 
-export default class App extends Component {
+const API_KEY = "AIzaSyCIieKZMX_fDEuA_LDYHFkRvYg-8XzTPIg";
+
+class App extends Component {
+ 
+  constructor(props) {
+    super(props);
+
+    this.state = { 
+      videos: [],
+      selectedVideo: null
+    };
+
+    this.videoSearch('surfboards');
+  }
+
+  videoSearch(term) {
+    YTSearch({key: API_KEY, term: term}, (videos) => {
+      console.log(videos);
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      });
+    });
+  }
   render() {
+    const videoSearch = _.debounce((term) => { this.videoSearch(term) }, 300);
+
     return (
-      <div>React simple starter</div>
+      <div>
+        <SearchBar onSearchTermChange={videoSearch} />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList 
+          onVideoSelect={selectedVideo => this.setState({selectedVideo})} 
+          videos={this.state.videos} />
+      </div>
     );
   }
 }
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -2,10 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-
 import App from './components/app';
 import reducers from './reducers';
-
+const API_KEY = "AIzaSyCIieKZMX_fDEuA_LDYHFkRvYg-8XzTPIg";
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
